5ea7094a11b1d5fd33e809c21ee1f756e1d258b8
gso1 <56320258+gso1@users.noreply.github.com>
2021-04-27 19:16:38 -0300

    Create decoder.v

---
+++ src/decoder.v
@@ -0,0 +1,18 @@
+`timescale 1ns/1ps
+
+module decoder (
+    sec_ones, sec_tens,min,
+    sec_ones_segs,sec_tens_segs,min_segs
+
+);
+
+    input [3:0] sec_ones, sec_tens,min;
+    output[6:0] sec_ones_segs,sec_tens_segs,min_segs;
+
+    bcd_seven sec_ones_module(.bcd(sec_ones),.seven(sec_ones_segs));
+
+    bcd_seven sec_tens_module(.bcd(sec_tens),.seven(sec_tens_segs));
+
+    bcd_seven min_module(.bcd(min),.seven(min_segs));
+
+endmodule
