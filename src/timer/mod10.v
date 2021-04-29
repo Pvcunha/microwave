f0e27205ea7c0c9f9f5a7da0abf8c1ef24808af4
Pedro Cunha <pvcunh4@gmail.com>
2021-04-29 11:50:53 -0300

    timer finalizado

--- src/timer/mod10.v
+++ src/timer/mod10.v
@@ -6,40 +6,37 @@ module mod10(
     input wire [3:0] data,
     input wire loadn, clrn, clk, en,
     output reg [3:0] out,
-    output reg tc, zero
+    output wire tc, output reg zero
 );
+
+    assign tc = (en & ~out[0] & ~out[1] & ~out[2] & ~out[3]);
 
     always @ (posedge clk or negedge clrn) begin
         
-        if(!clrn) begin
-            out = 0;
-            tc = 0;
-            zero = 0;
+        if(~clrn) begin
+            out <= 0;
+          
+            zero <= 0;
         end
-        else if(!loadn)
+        else if(~loadn) begin
             out <= data;
+            zero <= (data == 0)? 1 : 0;
+        end
         else begin
             if(en) begin
                 if(out == 1) begin
                     out <= out -1;
-                    zero <= 0;
-                    tc <= 1;
+                    zero <= 1;   
                 end
-
                 else if(out == 0) begin
                     out <= 9;
-                    zero <= 1;
-                    tc <= 0;
+                    zero <= 0;
                 end
-                
                 else begin
                     out <= out -1;
-                    zero <= 0;
-                    tc <= 0;
+                    zero <= 0;    
                 end
             end 
-            else
-                tc <= 0;
         end
     end
 
--- testbenches/tb_timer.v
+++ testbenches/tb_timer.v
@@ -1,64 +1,59 @@
 `timescale 1ns / 1ps
 
 
-module test_timer;
+module tb_timer;
     // Inputs
-    reg clk ;
-    reg clrn;
+  reg clk ;
+  reg clrn;
   reg loadn;
   reg en;
   reg [3:0] data;
-    // Output
-    wire[3:0] sec_ones, sec_tens, mins;
+  // Output
+  wire[3:0] sec_ones, sec_tens, mins;
   wire tc;
   wire zero;
-    // Instantiate the Timer
-  
-    timer uut(
-        .clk(clk), 
-        .clrn(clrn),
-    .loadn(loadn),
-    .en(en),
-    .data(data),
-    .sec_ones(sec_ones),
-    .sec_tens(sec_tens),
-    .mins(mins),
-    .zero(zero)
-    );
+  integer ticks = 0;
+  // Instantiate the Timer
+   timer t(
+      .clk(clk), 
+      .clrn(clrn),
+      .loadn(loadn),
+      .en(en),
+      .data(data),
+      .sec_ones(sec_ones),
+      .sec_tens(sec_tens),
+      .mins(mins),
+      .zero(zero)
+  );
+ 
+ 	initial begin
+ 	     clrn = 0;
+ 	     #2 clrn = 1;
+ 	end
+	
+	initial clk = 1;
 
-    initial begin
-        clrn = 0;
-        #1 clrn = 1;
-    end
+	always begin
+		#5 clk = ~clk; ticks++;
+		if(loadn & zero) $finish;
+	end
 
-always #1 clk <= ~clk;
+  	initial begin
+  	  	$dumpfile("vcd/timer.vcd");
+  	  	$dumpvars;
+		loadn=0;
+  	  	#10 data = 9;
+  	  	#10 data = 8;
+  	  	#10 loadn = 1; en = 1;
+  	end
 
-initial 
-  begin
-  $dumpfile("vcd/timer.vcd");
-  $dumpvars(0, test_timer);
-  clk<=1;
-  loadn<=0;
-  data<=4'b1001;
-  clrn<=1;
-  en<=1;
-  #2
-  loadn<=0;
-  data<=4'b0011;
-  clrn<=1;
-  en<=1;  
-  #2
-  loadn<=0;
-  data<=4'b0010;
-  clrn<=1;
-  en<=1;  
-  #2
-  loadn<=1;
-  data<=4'b0011;
-  clrn<=1;
-  en<=1;  
-  #500
-  $finish;
-  end
+  //always @(zero) begin
+  //  if(zero==1 & loadn==1) $finish;
+  //end
+
+
+  initial
+    #1 $monitor(sec_ones, sec_tens, mins);
+
  
 endmodule
