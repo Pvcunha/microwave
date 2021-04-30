cbe0b603a08c149b9b4efe9e251fcdaee3bf05bb
Pedro Cunha <pvcunh4@gmail.com>
2021-04-30 01:11:12 -0300

    testando o microwave junto

--- testbenches/tb_microwave.v
+++ testbenches/tb_microwave.v
@@ -2,7 +2,6 @@
 
 module tb_microwave;
 
-
     reg [9:0] key;
     reg clk;
     reg startn;
@@ -11,28 +10,35 @@ module tb_microwave;
     reg door_closed;
     wire [6:0] ones, tens, mins;
     wire mag;
+    integer ticks = 0;
     microwave m( .keypad(key), .clk(clk), .startn(startn), .stopn(stopn), .clearn(clearn), .door_closed(door_closed), .timer_done(timer_done), .mag(mag), .sec_ones_segs(ones), .sec_tens_segs(tens), .min_segs(mins) );
     
     initial 
         clk = 0;
 
-    always 
-        #1 clk = ~clk;
-
-    initial begin
-        clearn = 0;
-        #1 clearn = 1;
+    always begin 
+        #10 clk = ~clk;
+        ticks++;
+        if(ticks == 300)
+            $finish;
     end
 
     initial begin
-        startn = 1; stopn = 1; clearn = 1; door_closed = 1;
-        #1 key = 10'b0000000001; // 1
-        #1 key = 0;
-        #1 key = 10'b0000001000; // 4 
-        #1 key = 0;
-        #1 key = 10'b0000010000; // 5
-        #1 key = 0;
-        #1 startn = 0;
+        key = 0;
+        clearn = 0;
+        #10 clearn = 1;
+
+    end
+
+    initial begin
+        startn = 0; stopn = 1; clearn = 1; door_closed = 1;
+        #10 key = 10'b0000000001; // 1
+        #10 key = 0;
+        #10 key = 10'b0000001000; // 4 
+        #10 key = 0;
+        #10 key = 10'b0000010000; // 5
+        #10 key = 0;
+        #10 startn = 1;
     end
 
     initial begin
